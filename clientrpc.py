78db8291e6d4d124d1f1e8bca9f2f7e5ec90cd84
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-10-20 14:48:38 +0100

    falta adicionar dados na base de dados

--- clientrpc.py
+++ clientrpc.py
@@ -3,12 +3,15 @@ import xmlrpc.client
 # Conectar ao servidor RPC
 proxy = xmlrpc.client.ServerProxy('http://localhost:8000/')
 
-'''
-pais = 'Portugal'
-'''
-pais = input("Introduza o país da pessoa:")
+# Solicitar ao usuário que insira o país da pessoa
+pais = input("Introduza o país da pessoa: ")
+
+# Chamar a função no servidor RPC com base na entrada do usuário
 resultado = proxy.buscar_dados_por_pais(pais)
 
 # Imprimir os resultados
-for pessoa in resultado:
-    print(f"ID: {pessoa['id']}, Nome: {pessoa['first_name']} {pessoa['last_name']}, País: {pessoa['country']}, Idade: {pessoa['age']}")
+if resultado:
+    for pessoa in resultado:
+        print(f"ID: {pessoa['id']}, Nome: {pessoa['first_name']} {pessoa['last_name']}, País: {pessoa['country']}, Idade: {pessoa['age']}")
+else:
+    print(f"Nenhuma pessoa encontrada com o país: {pais}")
