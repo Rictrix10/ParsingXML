cbf7d5f8db315048550e53150c5676a1083be259
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-10-20 14:28:05 +0100

    Pesquisa paises

--- clientrpc.py
+++ clientrpc.py
@@ -3,8 +3,10 @@ import xmlrpc.client
 # Conectar ao servidor RPC
 proxy = xmlrpc.client.ServerProxy('http://localhost:8000/')
 
-# Solicitar dados para um país específico (por exemplo, "Brasil")
-pais = 'Brazil'
+'''
+pais = 'Portugal'
+'''
+pais = input("Introduza o país da pessoa:")
 resultado = proxy.buscar_dados_por_pais(pais)
 
 # Imprimir os resultados
--- serverrpc.py
+++ serverrpc.py
@@ -7,15 +7,15 @@ def buscar_dados_por_pais(pais):
     root = tree.getroot()
     
     resultado = []
-    for pessoa in root.findall('pessoa'):
-        country_element = pessoa.find('country')
+    for person in root.findall('Person'):
+        country_element = person.find('country')
         if country_element is not None and country_element.text == pais:
             resultado.append({
-                'id': pessoa.find('id').text,
-                'first_name': pessoa.find('first_name').text,
-                'last_name': pessoa.find('last_name').text,
+                'id': person.find('id').text,
+                'first_name': person.find('first_name').text,
+                'last_name': person.find('last_name').text,
                 'country': country_element.text,
-                'age': pessoa.find('age').text
+                'age': person.find('age').text
             })
     
     return resultado
